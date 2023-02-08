c8855f2d4b72080f069496571f70f6cece1e2dd6
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2023-02-08 16:41:40 +0600

    JQ-5
    trying spawn and exit

--- Assets/Kabir/Scripts/Level/LevelGenerator.cs
+++ Assets/Kabir/Scripts/Level/LevelGenerator.cs
@@ -1,34 +1,27 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public int currentLevel = 0;
-    public GameObject[] levels;
-
-
-
+    public Transform[] playerSpawnPoints;
+    public Transform[] exitPoints;
 
     private void Start()
     {
-        LoadLevel(currentLevel);
+        int randomPlayerSpawnIndex = Random.Range(0, playerSpawnPoints.Length);
+        int randomExitIndex = Random.Range(0, exitPoints.Length);
+
+        Transform playerSpawnPoint = playerSpawnPoints[randomPlayerSpawnIndex];
+        Transform exitPoint = exitPoints[randomExitIndex];
+
+        // Place the player at the random player spawn point
+        // Replace "player" with your player object
+        GameObject player = GameObject.FindWithTag("Player");
+        player.transform.position = playerSpawnPoint.position;
+
+        // Place the exit at the random exit point
+        // Replace "exit" with your exit object
+        GameObject exit = GameObject.FindWithTag("Exit");
+        exit.transform.position = exitPoint.position;
     }
-
-    private void LoadLevel(int levelIndex)
-    {
-        if (levelIndex >= 0 && levelIndex < levels.Length)
-        {
-            // Deactivate current level
-            if (currentLevel >= 0 && currentLevel < levels.Length)
-            {
-                levels[currentLevel].SetActive(false);
-            }
-
-            // Activate new level
-            currentLevel = levelIndex;
-            levels[currentLevel].SetActive(true);
-        }
-    }
-
-
 }
