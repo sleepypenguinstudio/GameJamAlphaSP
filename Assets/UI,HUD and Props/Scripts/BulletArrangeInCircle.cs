61308d456d0155e7565a8a0a720dcb136bd6e33f
Farhina Afrin <farhinafrin01@gmail.com>
2023-02-08 14:02:32 +0600

    hud

--- Assets/UI,HUD and Props/Scripts/BulletArrangeInCircle.cs
+++ Assets/UI,HUD and Props/Scripts/BulletArrangeInCircle.cs
@@ -2,23 +2,30 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BulletArrangeInCircle : MonoBehaviour
 {
-    public int numberOfObjects = 10;
+    [SerializeField]  int numberOfBullets  ;
     public float radius = 5f;
     public Vector2 center = new Vector2(0, 0);
-    public GameObject prefab;
+    public GameObject bullet;
 
     private GameObject[] objects;
+    public GameObject[] gunPanels;
+    private int currentPanelIndex = 0;
 
     private void Start()
     {
+       
+
         CreateCircle();
     }
 
     private void Update()
     {
+
+       
         if (Input.GetMouseButtonDown(0))
         {
             if (objects.Length > 0)
@@ -34,22 +41,68 @@ public class BulletArrangeInCircle : MonoBehaviour
         }
         else if (Input.GetMouseButtonDown(1))
         {
+            DestroyCircle();
+            CreateCircle();
+        }
+
+
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangePanel();
+            if (currentPanelIndex == 0)
+            {
+                numberOfBullets = 20;
+              
+            }
+            else if (currentPanelIndex == 1)
+            {
+                numberOfBullets = 6;
+               
+            }
+
+            else if (currentPanelIndex == 2)
+            {
+                numberOfBullets = 30;
+               
+            }
+            DestroyCircle();
             CreateCircle();
         }
     }
 
+
+    private void DestroyCircle()
+    {
+        if (objects != null  )
+        {
+            foreach (GameObject obj in objects)
+            {
+                Destroy(obj);
+            }
+        }
+    }
     private void CreateCircle()
     {
-        float angleStep = 360f / numberOfObjects;
-        objects = new GameObject[numberOfObjects];
-        for (int i = 0; i < numberOfObjects; i++)
+
+      
+        
+        float angleStep = 360f / numberOfBullets;
+        objects = new GameObject[numberOfBullets];
+        for (int i = 0; i < numberOfBullets; i++)
         {
             float angle = i * angleStep;
             Vector2 newPos = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)) * radius;
-            GameObject newObj = Instantiate(prefab, transform);
+            GameObject newObj = Instantiate(bullet, transform);
             RectTransform rectTransform = newObj.GetComponent<RectTransform>();
             rectTransform.anchoredPosition = newPos + center;
             objects[i] = newObj;
         }
+    }
+    private void ChangePanel()
+    {
+        gunPanels[currentPanelIndex].SetActive(false);
+        currentPanelIndex = (currentPanelIndex + 1) % gunPanels.Length;
+        gunPanels[currentPanelIndex].SetActive(true);
+        
     }
 }
