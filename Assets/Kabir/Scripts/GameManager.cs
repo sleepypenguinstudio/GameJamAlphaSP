2923246111a40232ed8668667544a2a8381d9d7a
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2023-02-12 17:16:08 +0600

    JQ-14

---
+++ Assets/Kabir/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            RestartGame();
+        }
+    }
+    public void RestartGame()
+    {
+        LevelManager.levelNo--;
+       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over!");
+        // Add any additional code for displaying a "game over" message or UI here.
+    }
+}
