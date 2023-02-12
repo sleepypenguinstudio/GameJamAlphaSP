26e07102247fbbde2dafd888cdf5afcd70a0fe55
Imtiaz Kabir <82272790+freeleech9@users.noreply.github.com>
2023-02-12 10:56:40 +0600

    JQ-5
    farhina worked

--- Assets/Kabir/Scripts/Level/Door/LevelManager.cs
+++ Assets/Kabir/Scripts/Level/Door/LevelManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LevelManager : MonoBehaviour
 {
     public static int levelNo;
+    public TMP_Text levelText;
 
     private void Start()
     {
         levelNo++;
         Debug.Log("Level No    "+levelNo);
+
+        ChangeLevelText();
     }
     private void Update()
     {
@@ -18,5 +23,10 @@ public class LevelManager : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+    }
+
+    public void ChangeLevelText()
+    {
+        levelText.text = "Level "+ levelNo.ToString();
     }
 }
